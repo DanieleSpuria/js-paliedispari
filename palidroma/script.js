18d4c7068e44d05d54051995268b94d48ecffe03
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-03-28 15:27:57 +0200

    add input and button

---
+++ palidroma/script.js
@@ -0,0 +1,11 @@
+/************************************************
+1. Input e button per inserire e salvare parola.
+2. 
+************************************************/
+
+
+const invia = document.getElementById('invia');
+const parola = document.getElementById('parola').value;
+
+
+console.log(invia, parola);
